1c34f02609c42e50b05f58b952bae0ecca48135e
susmote <admin@susmote.com>
2019-07-25 10:56:42 +0800

    git repo init

---
+++ src/main/java/com/susmote/vuehrsystem/VuehrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.susmote.vuehrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuehrsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuehrsystemApplication.class, args);
+    }
+
+}
