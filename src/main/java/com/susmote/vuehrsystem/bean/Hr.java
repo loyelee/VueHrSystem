20b642d29953fb8fc13ab128e2c527267b038dec
susmote <admin@susmote.com>
2019-07-25 17:47:44 +0800

    update all files

--- src/main/java/com/susmote/vuehrsystem/bean/Hr.java
+++ src/main/java/com/susmote/vuehrsystem/bean/Hr.java
@@ -1,18 +1,16 @@
-package com.susmote.vuehrsystem.bean;
+package org.sang.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /**
- * @author LoyeLee
- * @date 7/25/2019
+ * Created by sang on 2017/12/28.
  */
 public class Hr implements UserDetails {
     private Long id;
@@ -26,33 +24,58 @@ public class Hr implements UserDetails {
     private String remark;
     private List<Role> roles;
     private String userface;
-
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+    @Override
+    public String getUsername() {
+        return username;
+    }
     @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        for(Role role : roles){
-            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
         }
         return authorities;
+    }
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserface() {
+        return userface;
+    }
+
+    public void setUserface(String userface) {
+        this.userface = userface;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
     public Long getId() {
@@ -95,28 +118,13 @@ public class Hr implements UserDetails {
         this.address = address;
     }
 
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
-
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
-    @Override
-    public String getUsername() {
-        return username;
-    }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    @JsonIgnore
-    @Override
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
@@ -129,21 +137,5 @@ public class Hr implements UserDetails {
 
     public void setRemark(String remark) {
         this.remark = remark;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
-
-    public String getUserface() {
-        return userface;
-    }
-
-    public void setUserface(String userface) {
-        this.userface = userface;
     }
 }
